7bc7d8a838ac481626e1d7a5d63f7a87200cc540
jud-as <guilhermeferreira082@gmail.com>
2023-11-11 17:40:46 -0300

    Corrigidos erros de debug e alocação de "telefone"

--- SistemaGerenciamento.c
+++ SistemaGerenciamento.c
@@ -9,13 +9,14 @@ typedef struct servicos{
 
 }Servicos;
 
-typedef struct data {
+typedef struct data{
     int dia;
     int mes;
     int ano;
 }Data;
 
 typedef struct endereco {
+    int cep;
     int rua;
     int numero;    
     char bairro[30];
@@ -24,10 +25,10 @@ typedef struct endereco {
 
 typedef struct cliente{
     char nome[50];
-    Data data[10];
+    Data data[1];
     char email[50];
-    int telefone[30];
-    Endereco endereco [10];
+    long int telefone;
+    Endereco endereco[1];
 }Cliente;
 
 
@@ -35,28 +36,24 @@ Cliente solicitarInfosCliente() {
 
     Cliente c;
     int opc = 1;
-    int i = 0;
-
-    do
-    {
-        printf("\n-------------Cadastro Cliente -------------\n");
-        printf("\nNome: ");scanf("%s", &c.nome[i]);
-        printf("\nData de nascimento: ");
-        printf("\nDia: ");scanf("%d", &c.data[i].dia);
-        printf("Mês: ");scanf("%d", &c.data[i].mes);
-        printf("Ano: ");scanf("%d", &c.data[i].ano);
-        printf("\nE-mail: ");scanf("%s", &c.email[i]);
-        printf("Telefone: ");scanf("%d", &c.telefone[i]);
-        printf("Endereço: ");
-        printf("\nRua: ");scanf("%d", &c.endereco[i].rua);
-        printf("Número: ");scanf("%d", &c.endereco[i].numero);
-        printf("Bairro: ");scanf("%s", &c.endereco[i].bairro);
-        printf("UF: ");scanf("%s", &c.endereco[i].uf);
-        printf("\n\nSair - 0\nContinuar - 1\n");
-        printf("\nOPÇÃO: "); 
-        scanf("%d", &opc);
-        i++;
-    } while (opc != 0);
+    
+    
+    printf("\n-------------Cadastro Cliente -------------\n");
+    printf("\nNome: ");scanf("%s", c.nome);
+    printf("\nData de nascimento: ");
+    printf("\nDia: ");scanf("%d", &c.data[0].dia);
+    printf("Mês: ");scanf("%d", &c.data[0].mes);
+    printf("Ano: ");scanf("%d", &c.data[0].ano);
+    printf("\nE-mail: ");scanf("%s", c.email);
+    printf("Telefone: ");scanf("%ld", &c.telefone);
+    printf("Endereço: ");
+    printf("\nRua: ");scanf("%d", &c.endereco[0].rua);
+    printf("Número: ");scanf("%d", &c.endereco[0].numero);
+    printf("Bairro: ");scanf("%s", c.endereco[0].bairro);
+    printf("UF: ");scanf("%s", c.endereco[0].uf);
+    printf("\n\nSair - 0\nContinuar - 1\n");
+    printf("\nOPÇÃO: "); 
+    scanf("%d", &opc);
 
     return c;
 }
@@ -67,9 +64,8 @@ void imprimirClientes(Cliente c) {
     printf("\nNome: %s", c.nome);
     printf("\nData de nascimento: %d/%d/%d", c.data->dia, c.data->mes,c.data->ano);
     printf("\nE-mail: %s", c.email);
-    printf("\nTelefone: %d", c.telefone);
+    printf("\nTelefone: %ld", c.telefone);
     printf("\nEndereço:\nRua: %d\nNúmero: %d\nBairro: %s\nUF: %s", c.endereco->rua, c.endereco->numero, c.endereco->bairro, c.endereco->uf);
-
 }
 
 
@@ -78,7 +74,7 @@ void imprimirClientes(Cliente c) {
 int main(){
     setlocale(LC_ALL, "Portuguese_Brasil");
     int opcao;
-    
+    Cliente c;
     do
     {
         printf("\n0 - Sair\n1 - Área Clientes\n2 - Área Funcionários\n3 - Área Serviços");
@@ -94,7 +90,7 @@ int main(){
             break;
             
         case 1:
-            solicitarInfosCliente();
+            c = solicitarInfosCliente();
             break;
 
         case 2:
@@ -102,7 +98,7 @@ int main(){
             break;
 
         default:
-        printf("Operação inexistente.");
+            imprimirClientes(c);
             break;
         }
 
