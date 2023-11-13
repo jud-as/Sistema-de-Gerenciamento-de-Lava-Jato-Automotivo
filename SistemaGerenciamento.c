5ebc9686b9b63a041cdbe5cb6b380d2d0f43e1eb
Lucas Carvalho da Silveira <lucascasilveira@gmail.com>
2023-11-13 14:34:23 -0300

    Update SistemaGerenciamento.c

--- SistemaGerenciamento.c
+++ SistemaGerenciamento.c
@@ -2,41 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
-
+#include "listas.h"
 
 void clrscr()
 {
     system("@cls||clear");
 }
-
-typedef struct servicos{
-    char descricao[100];
-    int tipo;
-
-}Servicos;
-
-typedef struct data{
-    int dia;
-    int mes;
-    int ano;
-}Data;
-
-typedef struct endereco {
-    int cep;
-    int rua;
-    int numero;    
-    char bairro[30];
-    char uf[30];        
-}Endereco;
-
-typedef struct cliente{
-    char nome[50];
-    Data data;
-    char email[50];
-    long int telefone;
-    Endereco endereco;
-}Cliente;
-
 
 Cliente solicitarInfosCliente() {
 
